c968a8144a6c43eae3cf8f7d016f635154e173ba
TheFenix98 <108487428+TheFenix98@users.noreply.github.com>
2026-03-13 02:52:25 -0300

    cambio en el input

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ useEffect(() => {
           <input
             type="text"
             placeholder="Buscar..."
-            className="px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-gray-700 text-white text-sm"
+            className="w-full px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-gray-700 text-white text-sm"
             value={busqueda}
             onChange={(e) => {
               const valor = e.target.value;
